cabf186c6634dec2a69dcad6931c0950fb04aef7
Eric Barrett <neverloiter@gmail.com>
2021-12-02 15:55:05 -0330

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include "../NoMEM/src/NoMEM.h"
+#include "../NoGUI/src/GUI.h"
+#include "../NoMVC/src/Controller.h"
+#include "../NoPARSE/src/Parse.h"
+
+int main(int argc, char ** argv)
+{
+	if (argc > 1)
+	{
+		NoMVC::WindowConfig window = {1280, 1080, 60, 1/60, false, GRAY};
+		// Init Controller
+		std::shared_ptr< NoMVC::Controller > game = std::make_shared< NoMVC::Controller >();
+		game->changeWindow(window);
+		// Load Assets
+		std::shared_ptr< NoMEM::MEMManager > assets = std::make_shared< NoMEM::MEMManager >();
+		std::shared_ptr< NoGUI::Page > pg = NoGUI::loadPage(std::string(argv[1]), assets);
+		game->assets = *(assets.get());
+		// Init Models
+		std::shared_ptr< NoGUI::GUIManager > GUIModel = std::make_shared< NoGUI::GUIManager >(pg);
+		// Init View
+		std::shared_ptr< NoMVC::View > view = std::make_shared< NoMVC::View >(game.get(), game->getWindow());
+		view->addModel(GUIModel);
+		// Update Controller
+		game->changeScene(view);
+		// TODO: edit the GUI library so that we don't have to do this
+		for (std::shared_ptr< NoGUI::Element > elem : pg->getElements())
+		{
+			NoGUI::CDropDown dropdown = elem->getComponent< NoGUI::CDropDown >();
+			if ( dropdown.owned )
+			{
+				GUIModel->addPage(dropdown.options);
+			}
+		}
+		game->currentScene()->update();
+		// Run
+		int res = game->run();
+		
+		return res;
+	}
+	
+	return 0;
+}
