7f4d899a58bb46581f4004af31dc2aec0901365a
Eric Barrett <neverloiter@gmail.com>
2022-01-28 11:42:54 -0330

    Remove commented code

--- src/main.cpp
+++ src/main.cpp
@@ -16,23 +16,12 @@ int main(int argc, char ** argv)
 		std::shared_ptr< NoMEM::MEMManager > assets = std::make_shared< NoMEM::MEMManager >();
 		// Load Assets
 		std::shared_ptr< NoGUI::GUIManager > GUIModel = NoGUI::loadManager(std::string(argv[1]), assets);
-//		std::shared_ptr< NoGUI::Page > pg = NoGUI::loadPage(std::string(argv[1]), assets);
 		game->assets = *(assets.get());
-		// Init Models
-//		std::shared_ptr< NoGUI::GUIManager > GUIModel = std::make_shared< NoGUI::GUIManager >(pg);
 		// Init View
 		std::shared_ptr< NoMVC::View > view = std::make_shared< NoMVC::View >(game.get(), game->getWindow());
 		view->addModel(GUIModel);
 		// Update Controller
 		game->changeScene(view);
-//		for (std::shared_ptr< NoGUI::Element > elem : pg->getElements())
-//		{
-//			NoGUI::CDropDown dropdown = elem->getComponent< NoGUI::CDropDown >();
-//			if ( dropdown.owned )
-//			{
-//				GUIModel->addPage(dropdown.options);
-//			}
-//		}
 		game->currentScene()->update();
 		// Run
 		int res = game->run();
